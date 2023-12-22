fc0392818682f1ecfbab78c2b04941db94d7aa29
durgaprasanna123 <prasanna.25te@gmail.com>
2023-12-22 22:07:42 +0530

    pom.cml fole commited

--- src/test/java/pipelines/pipes/job1.java
+++ src/test/java/pipelines/pipes/job1.java
@@ -4,13 +4,16 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class job1 {
 	public static WebDriver driver;
-	@BeforeTest
+	
+	@BeforeMethod
 	public void LaunchBrowser() {
 		driver=new ChromeDriver();
 		
@@ -21,6 +24,7 @@ public class job1 {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		System.out.println(driver.getTitle());
+		 System.out.println(System.getProperty("java.class.path"));
 	}
 	@Test
 	public void test2() {
@@ -34,10 +38,10 @@ public class job1 {
 		driver.manage().window().maximize();
 		System.out.println(driver.getTitle());
 		}
-	@AfterTest
+	@AfterMethod
 	public void test4() {
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
 		driver.quit();
-	driver.get("driver was closed");
+	
 		
 }}
