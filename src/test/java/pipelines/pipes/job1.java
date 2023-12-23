68e53551a64ba6dee0763b2aa9da14277c72c0a0
durgaprasanna123 <prasanna.25te@gmail.com>
2023-12-23 13:10:27 +0530

    pom commit

--- src/test/java/pipelines/pipes/job1.java
+++ src/test/java/pipelines/pipes/job1.java
@@ -5,9 +5,9 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
+
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+
 import org.testng.annotations.Test;
 
 public class job1 {
